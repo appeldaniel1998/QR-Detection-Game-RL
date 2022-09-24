db6fcd3064723139d26948400ecec9dac95f2a93
Daniel Appel <appel.daniel1998@gmail.com>
2022-09-25 02:24:50 +0300

    Add Grade class to calculate grade + continued work on control API file

--- MainControlAPI.py
+++ MainControlAPI.py
@@ -1,37 +1,34 @@
-import os
-
-import numpy as np
-# ready to run example: PythonClient/car/hello_car.py
 import airsim
-import time
-import cv2
-import glob
-import pandas as pd
-import pathlib
+from CreateMap import createMap
+import numpy as np
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     # init airsim and drones
     client = airsim.MultirotorClient()  # connect to the simulator
     client.confirmConnection()
     client.reset()
 
+    client = createMap(client)
+
     client.enableApiControl(True)  # enable API control on Drone0
-    client.armDisarm(True)  # arm Drone0
+    client.armDisarm(True)  # arm Drone
 
-    client.takeoffAsync().join()  # let Drone0 take-off
-    client.moveToPositionAsync(0, 0, -1, 1).join()
-    client.hoverAsync().join()
+    # client.simSetObjectMaterialFromTexture("testCube1_2", "C:/Users/appel/PycharmProjects/pythonProject6/ArucoImages/aruco1.jpg")
 
-    vx = 0
-    vy = 5
-    vertical_speed = 0
-    camera_heading = 180
-    client.moveByVelocityAsync(vx, vy, vertical_speed, 1, airsim.DrivetrainType.MaxDegreeOfFreedom,
-                               airsim.YawMode(False, camera_heading)).join()
-    client.hoverAsync().join()
-
-    state = client.getMultirotorState()
-    print(state)
-
-    collision = client.simGetCollisionInfo()
-    print(collision)
+    # client.takeoffAsync().join()  # let Drone0 take-off
+    # client.moveToPositionAsync(0, 0, -1, 1).join()
+    # client.hoverAsync().join()
+    #
+    # xSpeed = 0
+    # ySpeed = 5
+    # zSpeed = 0  # Vertical
+    # camera_heading = 180
+    # client.moveByVelocityAsync(xSpeed, ySpeed, zSpeed, 1, airsim.DrivetrainType.MaxDegreeOfFreedom,
+    #                            airsim.YawMode(False, camera_heading)).join()
+    # client.hoverAsync().join()
+    #
+    # state = client.getMultirotorState()
+    # print(state)
+    #
+    # collision = client.simGetCollisionInfo()
+    # print(collision)
